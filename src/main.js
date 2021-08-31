207aab094a9e329a1a7c691593b2a55c813f7be4
Liftoff-KA <84321034+Liftoff-KA@users.noreply.github.com>
2021-08-31 09:49:56 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
+var x = document.cookie.split(";");
+var fkey = "";
+for (var i = 0; i < x.length; i++) {
+    var z = x[i].split("=");
+    if (z[0] === " fkey") {
+        fkey = z[1];
+    }
+};
 
+export var fkey;
