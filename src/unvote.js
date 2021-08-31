fc76f69582c5363e2f601932bc7caf85984d8199
Liftoff-KA <84321034+Liftoff-KA@users.noreply.github.com>
2021-08-31 09:51:32 +0530

    Update unvote.js

--- src/unvote.js
+++ src/unvote.js
@@ -1,6 +1,8 @@
 //Stuff that adds the unvote button
 //Took me a lot of time but worthwhile :D
 
+import fkey from "https://raw.githubusercontent.com/Liftoff-KA/Ultimate-KA-Extension/main/src/main.js";
+
 var num_array = ["1","2","3","4","5","6","7","8","9","0"];
 
 //This if statement is required to seperate the individual functions for pages. It will become important later
@@ -48,15 +50,6 @@ if(num_array.includes(window.location.href.split("/")[5])){
         unvote_button.style.paddingRight = "14px";
     })
 
-
-    var x = document.cookie.split(";");
-    var fkey = "";
-    for (var i = 0; i < x.length; i++) {
-        var z = x[i].split("=");
-        if (z[0] === " fkey") {
-            fkey = z[1];
-        }
-    };
 
 
     function removeVote(url) {
