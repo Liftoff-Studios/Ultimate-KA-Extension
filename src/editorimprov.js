f3a7426b5e250ae5151113a3c3fb608f20357d2f
Liftoff-KA <84321034+Liftoff-KA@users.noreply.github.com>
2021-08-31 11:46:06 +0530

    Update editorimprov.js

--- src/editorimprov.js
+++ src/editorimprov.js
@@ -1,1 +1,10 @@
 
+//import fkey from "https://raw.githubusercontent.com/Liftoff-KA/Ultimate-KA-Extension/main/src/main.js"; //Not required here
+
+var num_array = ["1","2","3","4","5","6","7","8","9","0"];
+
+//This if statement is required to seperate the individual functions for pages. It will become important later
+if(num_array.includes(window.location.href.split("/")[5])){
+  
+  
+}
